fd97fedb63c9b7e53f20e4e88b0943289c389f8f
Raghu Vikas Reddy <raghuvikasreddy@Raghus-Mac-mini.local>
2026-03-21 17:18:32 +0530

    bug fix on rock paper scissors

--- day4/rock_paper_scissors.py
+++ day4/rock_paper_scissors.py
@@ -32,14 +32,16 @@ print("Welcome to the Rock Paper Scissors game!")
 choice = [rock, paper, scissors]
 index = random.randint(0, len(choice)-1)
 
-player_choice= int(input("What do you choose? \n0 for ROCK \n1 for PAPER \n2 for SCISSOR \n"))
 computer_choice = index
+player_choice= int(input("What do you choose? \n0 for ROCK \n1 for PAPER \n2 for SCISSOR \n"))
+if player_choice >= 0 and player_choice <= 2:
+    print(choice[player_choice])
+    print("Computer chose:")
+    print(choice[computer_choice])
 
-print(choice[player_choice])
-print("Computer chose:")
-print(choice[computer_choice])
-
-if player_choice == computer_choice:
+if player_choice > 2 or player_choice < 0:
+    print("Invalid Choice! Game Over!")
+elif player_choice == computer_choice:
     print("It's a tie!")
 elif player_choice == 0 and computer_choice ==1:
     print("You lose!")
@@ -53,5 +55,4 @@ elif player_choice == 1 and computer_choice == 2:
     print("You lose!")
 elif player_choice == 2 and computer_choice == 1:
     print("You win!")
-else:
-    print("Invalid Choice! Game Over!")
+
